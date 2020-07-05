c5ebd1e45bf0a643fc86bfc218770c291aa800e7
flywind <xzsflywind@gmail.com>
2020-07-05 18:01:40 +0800

    initial

---
+++ src/syscall.nim
@@ -0,0 +1,10 @@
+import syscall/types
+export types
+
+
+proc syscall*(n: clong, a1: clong): clong {.importc: "syscall", header: "syscall.h".}
+proc syscall*(n: clong, a1, a2: clong): clong {.importc: "syscall", header: "syscall.h".} 
+proc syscall*(n: clong, a1, a2, a3: clong): clong {.importc: "syscall", header: "syscall.h".}
+proc syscall*(n: clong, a1, a2, a3, a4: clong): clong {.importc: "syscall", header: "syscall.h".}
+proc syscall*(n: clong, a1, a2, a3, a4, a5: clong): clong {.importc: "syscall", header: "syscall.h".}
+proc syscall*(n: clong, a1, a2, a3, a4, a5, a6: clong): clong {.importc: "syscall", header: "syscall.h".}
